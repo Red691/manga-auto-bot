3c13ce7e1918e79be9baf84147117b8c7f98ad51
Red691 <kumarnikhil05848@gmail.com>
2026-01-20 00:34:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ import os
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+MAIN_CHANNEL = os.environ.get("-1002769680577")
+
+if MAIN_CHANNEL is None:
+    raise ValueError("MAIN_CHANNEL environment variable not set")
+
+MAIN_CHANNEL = int(MAIN_CHANNEL)
